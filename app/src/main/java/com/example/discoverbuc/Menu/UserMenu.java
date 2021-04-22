8d9429865f50e3c7ff44751121a9a38e427a6572
Catalin Barabas <barabas.catalin@gmail.com>
2021-04-22 22:14:22 +0300

    retrieving data from firebase successfully, but not updating locations cards

--- app/src/main/java/com/example/discoverbuc/Menu/UserMenu.java
+++ app/src/main/java/com/example/discoverbuc/Menu/UserMenu.java
@@ -1,16 +1,26 @@
 package com.example.discoverbuc.Menu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.discoverbuc.Menu.HelperClasses.AdapterHelperClass;
 import com.example.discoverbuc.Menu.HelperClasses.CardHelperClass;
 import com.example.discoverbuc.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class UserMenu extends AppCompatActivity {
 
@@ -33,14 +43,45 @@ public class UserMenu extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
-        ArrayList<CardHelperClass> locations = new ArrayList<>();
+        ArrayList<CardHelperClass> locationsArray = new ArrayList<>();
 
-        locations.add(new CardHelperClass(R.drawable.palatul_parlamentului, 5, "Palatu Parlamentului", "asdjahj dajh dad kja dkjasd asjd asd a"));
-        locations.add(new CardHelperClass(R.drawable.palatul_parlamentului, 3.5f, "Titlu 2", "asasddsfa dkjasd asjd asd a"));
-        locations.add(new CardHelperClass(R.drawable.palatul_parlamentului, 2.5f, "Titlu 3", "asdjahasdasd dkjasd asjd asd a"));
-        locations.add(new CardHelperClass(R.drawable.palatul_parlamentului, 1f, "Titlu 4", "asdjahj dajh dad kasd asdasdja dkjasd aasd asd asd as ddassjd asd a"));
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("locations");
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-        adapter = new AdapterHelperClass(locations);
+                Iterable<DataSnapshot> categories = snapshot.getChildren();
+                Iterator<DataSnapshot> category = categories.iterator();
+
+                while(category.hasNext()){
+                    DataSnapshot next = (DataSnapshot) category.next();
+                    Iterable<DataSnapshot> locations = next.getChildren();
+                    Iterator<DataSnapshot> locationDetails = locations.iterator();
+                    while(locationDetails.hasNext()){
+                        DataSnapshot detail = (DataSnapshot) locationDetails.next();
+                        String title = detail.child("name").getValue(String.class);
+                        String desc = detail.child("desc").getValue(String.class);
+                        float rating = detail.child("rating").getValue(float.class);
+                        String imageName = detail.child("image").getValue(String.class);
+                        int imageLoc = getApplicationContext().getResources().getIdentifier(imageName, "drawable", getApplicationContext().getPackageName());
+                        locationsArray.add(new CardHelperClass(imageLoc, rating, title, desc));
+                        Log.d("Message", "MERGE PANA AICI");
+                        Log.d("name", title);
+                        Log.d("imageloc", imageName);
+                        Log.d("desc", desc);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+
+        ref.addListenerForSingleValueEvent(valueEventListener);
+
+        adapter = new AdapterHelperClass(locationsArray);
         recyclerView.setAdapter(adapter);
 
     }
